1d9a52e0f8f4de7bd5cd9d254f3df0ed846d0929
Thorulf Neustrup <thorulf@live.dk>
2021-03-14 12:36:14 +0100

    Revert "Updating fork"

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-#  ↗![icon](https://cdn.discordapp.com/avatars/819606905735479356/f30474b0aa66b3541bfdacf5bad5783a.png)↖ Boni-Py
-Boni-Py is a discord bot for the one and only Slambonis server. Although he may look simple, there is a few things going on under that hood of his. He can create temperary voice channels and uses Discords new slash commands to the best of there abilities.
+# Boni-Py
+Boni bot but he is coded in python and uses slash commands!
 
-## How to use
-To run this project you need discord.py and slash commands
+~~Hahaha I can change the readme~~
 
-There is a `sample.env` that you must fill out and rename to `.env`
-
-### 🚧 This read me is still being made 🚧
-
+Click [here](https://www.youtube.com/watch?v=dQw4w9WgXcQ) to get a tutorial
--- behaviors/copyCat.py
+++ behaviors/copyCat.py
@@ -1,26 +1,15 @@
 import discord
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-guilds = []
-guilds.append(int(os.getenv('GUILD_ID')))
-
 
 class copyCat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="say", description='Enter a message and I will repeat it :)', guild_ids=guilds)
+    @cog_ext.cog_slash(name="say", description='Enter a message and I will repeat it :)', guild_ids = [443884809484238848])
     async def group_say(self, ctx: SlashContext, message: str):
-        if ctx.author.id == 324352543612469258 or ctx.author.id == 329855467591434250:
-            await ctx.respond(await ctx.send_hidden("Message sent successfully"))
-            await ctx.send(message)
-        else:
-            await ctx.respond(await ctx.send_hidden("No ♥"))
-
+        await ctx.send(message)
+            
 
 def setup(bot):
     bot.add_cog(copyCat(bot))
